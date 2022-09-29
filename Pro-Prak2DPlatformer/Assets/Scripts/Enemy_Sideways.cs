b24e02097020ead01d9c0cb28e8a7802a6410a0a
BerkantA21 <berkant5@hotmail.com>
2022-09-29 20:37:34 +0200

    obstacle script aangemaakt
    Moet alleen nog de damage van de obstacle bewerken

---
+++ Pro-Prak2DPlatformer/Assets/Scripts/Enemy_Sideways.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Sideways : MonoBehaviour
+{
+    [SerializeField] private float movementDistance;
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    private bool movingLeft;
+    private float leftEdge;
+    private float rightEdge;
+
+    private void Awake()
+    {
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.x + movementDistance;
+    }
+
+    private void Update()
+    {
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+                movingLeft = false;
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+                movingLeft = true;
+        }
+    }
+}
