aad5ce6e15533015e863a95497a68ee15beb29ca
Thomas Happé <333893@student.mboutrecht.nl>
2022-09-30 11:24:04 +0200

    Sprite Flip
    Sprite of enemy now flips when turning the other way

--- Pro-Prak2DPlatformer/Assets/Scripts/EnemyAI.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/EnemyAI.cs
@@ -6,11 +6,23 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private GameObject[] waypoints;
     private int currentWaypointIndex = 0;
-
+    private SpriteRenderer _renderer;
     [SerializeField] private float speed = 2f;
-
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
+        if (currentWaypointIndex == 0)
+        {
+            _renderer.flipX = false;
+        }
+        else if (currentWaypointIndex == 1)
+        {
+            _renderer.flipX = true;
+        }
+
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
         {
             currentWaypointIndex++;
