12301e0dedcf8fbac415b64ad216386309be2b5b
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2022-10-01 14:46:32 +0200

    Fix EndScreen

--- Pro-Prak2DPlatformer/Assets/Scripts/Control.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/Control.cs
@@ -5,20 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Control : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void ResetTheGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("StartScreen");
         print("De button werkt hoor!");
     }
 }
