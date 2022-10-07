c1b7a4d8b08e409f9f0c3a2bf99c42d5651d0455
Thomas Happé <333893@student.mboutrecht.nl>
2022-10-07 15:12:41 +0200

    Dash and Crouch
    Added the dash and crouch animation (LCTRL for Crouch and RCTRL for Dash (while crouched))

---
+++ Pro-Prak2DPlatformer/Assets/Scripts/Crouch.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
+public class Crouch : MonoBehaviour
+{
+    private Animator ani;
+    public bool IsCrouch = false;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            IsCrouch = !IsCrouch;
+            CrouchFunction();
+        }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            DashFunction();
+        }
+    }
+
+    void CrouchFunction()
+    {
+        if (IsCrouch == true)
+        {
+            ani.SetBool("crouch", true);
+        }
+        else
+        {
+            ani.SetBool("crouch", false);
+        }
+    }
+
+    void DashFunction()
+    {
+        if (IsCrouch == true)
+        {
+            ani.SetTrigger("dash");
+        } 
+        else
+        {
+            ani.ResetTrigger("dash");
+        }
+    }
+}
