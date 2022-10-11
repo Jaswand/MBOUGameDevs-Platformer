52f30e490947d7447e598f3c20a743f4a5608292
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2022-10-11 14:32:49 +0200

    added StaminaBar

--- Pro-Prak2DPlatformer/Assets/Scripts/Player.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/Player.cs
@@ -1,33 +1,85 @@
+ 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+ 
 public class Player : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Image healthfill;  
+    [SerializeField] private Gradient gradient;
 
-    public HealthBar healthBar;
-    // Start is called before the first frame update
-    void Start()
+    private float currentHealth;
+    private float maxHealth;
+    private bool infinitHealth;
+    private bool isFullHealth;
+
+  public void SetInitialHealth(float health)
     {
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        healthBar.maxValue = health;
+        healthBar.value = health;
+
+        healthfill.color = gradient.Evaluate(1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        currentHealth = maxHealth;
+        SetInitialHealth(maxHealth);
+    }
+
+    public void UpdateHealth(float currentHealth)
+    {
+        healthBar.value = currentHealth;
+
+        healthfill.color = gradient.Evaluate(healthBar.normalizedValue);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (infinitHealth == false)
         {
-            TakeDamage(20);
+            currentHealth -= damage;
+
+            UpdateHealth(currentHealth);
+
+            if(currentHealth <= 1)
+            {
+                //spawnManager.OnPlayerDeath();
+                Destroy(gameObject);
+                Debug.Log("You are dead");
+            }
+
+            if (currentHealth < 20)
+            {
+                Debug.Log("Your health is very low");
+            }
+        }
+        else
+           return;
+    }
+
+    public void AddHealth(float healthamount)
+    {
+        if (currentHealth < maxHealth && isFullHealth == false)
+        {
+            currentHealth += healthamount;
+            UpdateHealth(currentHealth);
+            Debug.Log("Added " +healthamount+ "Health Points");
+        }
+        else
+        {
+            Debug.Log("Health is full");
         }
     }
 
-    void TakeDamage(int damage)
+    private void Update()
     {
-        currentHealth -= damage;
+       if (Input.GetKeyDown(KeyCode.Space))
+            StaminaBar.instance.UseStamina(15);
 
-        healthBar.SetHealth(currentHealth);
+        
     }
 }
---
+++ Pro-Prak2DPlatformer/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider staminaBar;
+
+    private int maxStamina = 100;
+    private int currentStamina;
+
+    private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
+    private Coroutine regen;
+
+    public static StaminaBar instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = maxStamina;
+    }
+
+    public void UseStamina(int amount)
+    {
+        if(currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+
+            if (regen != null)
+                StopCoroutine(regen);
+
+            regen = StartCoroutine(RegenStamina());
+        }
+        else
+        {
+            Debug.Log("Not enough stamina");
+        }
+
+    }
+
+    private IEnumerator RegenStamina()
+    {
+        yield return new WaitForSeconds(2);
+         
+        while (currentStamina < maxStamina)
+        {
+            currentStamina += maxStamina / 100;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+        }
+        regen = null;
+    }   
+}
