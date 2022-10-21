b9787f8ea78a3f8f118ee5300f43c332727c29e4
Berkant Ates <321906@student.mboutrecht.nl>
2022-10-21 15:52:45 +0200

    Revert "Merge branch 'Advanced-Obstacles' into Development"
    This reverts commit 95c29a0f1fddb5682838c7b93a108ab1ea88f458, reversing
    changes made to 44d2bdcb8909b9919ff50cd3d176e267a416001e.

--- Pro-Prak2DPlatformer/Assets/Scripts/ScoreScript.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/ScoreScript.cs
@@ -17,8 +17,9 @@ public class ScoreScript : MonoBehaviour
         MyScore.text = "Score: " + ScoreNum;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.tag == "Coin")
         {
             coinSoundEffect.Play();
