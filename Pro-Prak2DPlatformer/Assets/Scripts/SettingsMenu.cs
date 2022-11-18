9bc538a48eab21e2bb04ffa55d9cb54432fb3f3e
Mees <334104@student.mboutrecht.nl>
2022-11-18 10:02:37 +0100

    Settings menu beta
    Settings added + functional, but it doesn't save the preferred settings yet

---
+++ Pro-Prak2DPlatformer/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("Volume", volume);
+    }
+}
