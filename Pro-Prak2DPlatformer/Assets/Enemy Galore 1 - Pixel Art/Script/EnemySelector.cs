14b4b7638b240999b942cf35ecc10e2edba760df
Jaswand Soekhoe <jp.soekhoe@outlook.com>
2022-11-25 13:36:27 +0100

    New Asset Added
    - Enemy Galore 1 - Pixel Art

---
+++ Pro-Prak2DPlatformer/Assets/Enemy Galore 1 - Pixel Art/Script/EnemySelector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySelector : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] private Button[] allAnimationButtons;
+    [SerializeField] private Button[] enableAnimationButtons;
+
+    [Header("Game Objects")]
+    [SerializeField] private GameObject[] allEnemyGOs;
+    [SerializeField] private GameObject enableEnemy;
+
+    [Header("Text")]
+    [SerializeField] private Text enemyName;
+
+    public void Start()
+    {
+        enemyName.text = "";
+    }
+    public void ChangeEnemies()
+    {
+        _DisableAllEnemies();
+        _DisableAllButtons();
+        _EnableEnemy();
+        _EnableButton();
+        _Rename();
+    }
+
+    public void _Rename()
+    {
+        enemyName.text = enableEnemy.gameObject.name;
+    }
+
+    public void _EnableButton()
+    {
+        for (int i = 0; i < enableAnimationButtons.Length; i++)
+        {
+            enableAnimationButtons[i].interactable = true;
+        }
+    }
+    public void _DisableAllButtons()
+    {
+        for (int i = 0; i < allAnimationButtons.Length; i++)
+        {
+            allAnimationButtons[i].interactable = false;
+        }
+    }
+
+    public void _EnableEnemy()
+    {
+        enableEnemy.SetActive(true);
+    }
+    public void _DisableAllEnemies()
+    {
+        for (int i = 0; i < allEnemyGOs.Length; i++)
+        {
+            allEnemyGOs[i].gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Pro-Prak2DPlatformer/Assets/Enemy Galore 1 - Pixel Art/Script/EnemyTest.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+    [SerializeField] private Animator[] EnemyAnims;
+
+    public void Animation_1_Idle()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is Idling");
+            }
+        }
+    }
+    public void Animation_2_Run()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is Running");
+
+            }
+        }
+    }
+    public void Animation_3_Hit()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetTrigger("Hit");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is being Hit");
+            }
+        }
+    }
+    public void Animation_4_Death()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetTrigger("Death");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " has died");
+            }
+        }
+    }
+    public void Animation_5_Ability()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetBool("Ability", true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using its First Ability");
+            }
+        }
+    }
+    public void Animation_5_Ability2()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetBool("Ability 2", true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using its Second Ability");
+            }
+        }
+    }
+    public void Animation_5_Ability3()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetBool("Ability 3", true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using its Third Ability");
+            }
+        }
+    }
+    public void Animation_6_Attack()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetTrigger("Attack");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using using its Primary Attack");
+            }
+        }
+    }
+
+    public void Animation_7_Attack2()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetTrigger("Attack 2");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using using its Secondary Attack");
+            }
+        }
+    }
+    public void Animation_8_Attack3()
+    {
+        for (int i = 0; i < EnemyAnims.Length; i++)
+        {
+            if (EnemyAnims[i].gameObject.activeSelf == true)
+            {
+                EnemyAnims[i].SetBool("Run", false);
+                EnemyAnims[i].SetTrigger("Attack 3");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using using its Tertiary Attack");
+            }
+        }
+    }
+}
