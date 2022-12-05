62f3640524e9f20c9650905a9f8edfe4f9edb5ef
Berkant Ates <321906@student.mboutrecht.nl>
2022-12-05 15:04:05 +0100

    spikeheadshooter werkt compleet
    werkt compleet alleen moet de range nog toevoegen.

--- Pro-Prak2DPlatformer/Assets/Scripts/EnemyBulletScript.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/EnemyBulletScript.cs
@@ -7,6 +7,7 @@ public class EnemyBulletScript : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     public float force;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,22 @@ public class EnemyBulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerLife.Instance.Damage(20);
+            //Debug.Log(PlayerLife.Instance.health);
+        }
+        if(other.gameObject.tag != "Enemy")
+            Destroy(gameObject);
     }
 }
--- Pro-Prak2DPlatformer/Assets/Scripts/EnemyShooting.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/EnemyShooting.cs
@@ -8,16 +8,19 @@ public class EnemyShooting : MonoBehaviour
     public Transform bulletPos;
 
     private float timer;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
 
         if (timer > 2)
         {
