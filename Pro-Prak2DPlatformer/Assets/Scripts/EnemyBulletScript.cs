63d04106602e8d1738faf2784511b20438494184
Berkant Ates <321906@student.mboutrecht.nl>
2022-12-05 14:16:15 +0100

    fixed 1 bug and also testing in TestLevel

--- Pro-Prak2DPlatformer/Assets/Scripts/EnemyBulletScript.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/EnemyBulletScript.cs
@@ -7,7 +7,6 @@ public class EnemyBulletScript : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     public float force;
-    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,27 +16,13 @@ public class EnemyBulletScript : MonoBehaviour
         Vector3 direction = player.transform.position - transform.position;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
 
-        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = quaternion.Euler(0, 0, rot + 90);
+        float rot = Mathf.Atan2(-direction.y, -direction.x) *Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += timer.deltaTime;
-
-        if(timer > 10)
-        {
-            Destroy(GameObject);
-        }
-    }
-
-    void OntriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            //other.gameObject.GetComponent<playerHealth>().health -= 10;
-            Destroy(gameObject);
-        }
+        
     }
 }
--- Pro-Prak2DPlatformer/Assets/Scripts/EnemyShooting.cs
+++ Pro-Prak2DPlatformer/Assets/Scripts/EnemyShooting.cs
@@ -8,29 +8,21 @@ public class EnemyShooting : MonoBehaviour
     public Transform bulletPos;
 
     private float timer;
-    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("player");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
 
-        float distance = Vector2.Distance(transform.position, player.transform.position);
-        Debug.Log(distance);
-
-        if(distance < 4)
+        if (timer > 2)
         {
-            timer += Time.deltaTime;
-
-            if (timer > 2)
-            {
-                timer = 0;
-                shoot();
-            }
+            timer = 0;
+            shoot();
         }
     }
 
